5dc851f84908db81c0c8c7f60b1c363ed14f7c5c
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-11-19 22:35:38 -0600

    Phase3

--- Heister.cs
+++ Heister.cs
@@ -7,5 +7,20 @@ namespace HeistExercise
         public string Name { get; set; }
         public int SkillLevel { get; set; }
         public decimal CourageFactor { get; set; }
+
+        // Collect several team members' information.
+        public Heister(string name, int skillLevel, decimal courageFactor)
+        {
+            Name = name;
+            SkillLevel = skillLevel;
+            CourageFactor = courageFactor;
+        }
+
+        public Heister()
+        {
+
+        }
     }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -10,51 +10,108 @@ namespace HeistExercise
             // Phase 1
             Console.WriteLine("Plan Your Heist");
             Console.WriteLine();
-            Heister teamMember;
+            // Heister teamMember;
 
-            Console.WriteLine("What is the team member's name?");
-            string name = Console.ReadLine();
+            // Create a way to store several team members.
+            List<Heister> NewHeisters = new List<Heister>();
 
-            Console.WriteLine("What's your tema member's skill level?");
-            string skillLevelString = Console.ReadLine();
-            int skillLevel;
-
-            try
+            bool wantsToAddName = true;
+            while (wantsToAddName)
             {
-                skillLevel = int.Parse(skillLevelString);
+                Heister NewHeister = new Heister();
+
+                Console.WriteLine("What is the team member's name?");
+                string name = Console.ReadLine();
+                // Stop collecting team members when a blank name is entered.
+                if (name == "")
+                {
+                    NewHeister = null;
+                    break;
+                }
+                else
+                {
+                    NewHeister.Name = name;
+                }
+                bool hasValidSkillLevel = false;
+                while (!hasValidSkillLevel)
+                {
+
+                    Console.WriteLine("What's your tema member's skill level?");
+                    string skillLevelString = Console.ReadLine();
+                    // int skillLevel;
+                    try
+                    {
+                        NewHeister.SkillLevel = int.Parse(skillLevelString);
+                        hasValidSkillLevel = true;
+                    }
+                    catch (Exception)
+                    // ex prints message to the console when called. 
+                    {
+                        Console.WriteLine($"{skillLevelString} is not a valid skill level.");
+
+                    }
+                }
+                bool hasValidCourageFactor = false;
+                while (!hasValidCourageFactor)
+                {
+
+                    Console.WriteLine("What's your team member's courage factor?");
+                    string courageFactorString = Console.ReadLine();
+                    // decimal courageFactor;
+
+                    try
+                    {
+                        NewHeister.CourageFactor = decimal.Parse(courageFactorString);
+                        hasValidCourageFactor = true;
+                    }
+                    catch (Exception)
+                    {
+                        // M means money symbol so the compiler is ok with it. 
+                        Console.WriteLine($"{courageFactorString} is not a valid courage factor.");
+
+                    }
+
+                    NewHeisters.Add(NewHeister);
+                }
+
             }
-            catch (Exception ex)
-            // ex prints message to the console when called. 
-            {
-                Console.WriteLine($"{skillLevelString} is not a valid skill level. Using a default value of 10");
-                skillLevel = 10;
-            }
-            Console.WriteLine("What's your team member's courage factor?");
-            string courageFactorString = Console.ReadLine();
-            decimal courageFactor;
+            // Display a message containing the number of members of the team.
+            Console.WriteLine(NewHeisters.Count);
 
-            try
+            // Display each team member's information.
+
+            Console.Clear();
+
+            foreach (Heister TeamMember in NewHeisters)
             {
-                courageFactor = decimal.Parse(courageFactorString);
-            }
-            catch (Exception)
-            {
-                // M means money symbol so the compiler is ok with it. 
-                Console.WriteLine($"{courageFactorString} is not a valid courage factor. Using a default value of 1");
-                courageFactor = 1.0M;
+                Console.WriteLine($"Name: {TeamMember.Name} Skill Level: {TeamMember.SkillLevel} Courage Factor: {TeamMember.CourageFactor}");
             }
 
-            teamMember = new Heister()
+            // Stop displaying each team member's information.
+
+            Console.ReadLine();
+            Console.Clear();
+            // Store a value for the bank's difficulty level. Set this value to 100.
+            int bankDifficulty = 100;
+
+
+            // Sum the skill levels of the team.Save that number.
+            int combineSkillLevel = 0;
+            foreach (Heister item in NewHeisters)
             {
-                Name = name,
-                SkillLevel = skillLevel,
-                CourageFactor = courageFactor
+                combineSkillLevel += item.SkillLevel;
+            }
 
-            };
-            Console.WriteLine($"Name: {teamMember.Name}");
-            Console.WriteLine($"Skill Level: {teamMember.SkillLevel}");
-            Console.WriteLine($"Courage Factor: {teamMember.CourageFactor}");
-
+            // Compare the number with the bank's difficulty level. 
+            // If the team's skill level is greater than or equal to the bank's difficulty level, Display a success message, otherwise display a failure message
+            if (combineSkillLevel >= bankDifficulty)
+            {
+                Console.WriteLine("Great Success!");
+            }
+            else
+            {
+                Console.WriteLine("Think of a career change!");
+            }
         }
     }
 }
